4fa35486e42958129ed9c484a8b507056802849c
Muthu Kumar <muthukumar@thefeathers.in>
2018-09-23 06:44:26 +0530

    [editor] Tab is captured in editor and tabs are now 4 spaces!

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,32 @@ window.addEventListener(
 	"load",
 	function() {
 
+		/* Can't bother: https://stackoverflow.com/a/18303822 */
+		document
+		.querySelector("textarea")
+		.addEventListener('keydown',function(e) {
+			if(e.keyCode === 9) { // tab was pressed
+				// get caret position/selection
+				var start = this.selectionStart;
+				var end = this.selectionEnd;
+		
+				var target = e.target;
+				var value = target.value;
+		
+				// set textarea value to:
+				// text before caret + tab + text after caret
+				target.value = value.substring(0, start)
+							+ "\t"
+							+ value.substring(end);
+		
+				// put caret at right position again (add one for the tab)
+				this.selectionStart = this.selectionEnd = start + 1;
+		
+				// prevent the focus lose
+				e.preventDefault();
+			}
+		}, false);
+
 		document.addEventListener('keydown', function(e) {
 
 			// Ctrl + Shift + N
